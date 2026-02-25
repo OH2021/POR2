77126618a70dfb4bc6d33489831390d00613b7a6
OH2021 <77490624+OH2021@users.noreply.github.com>
2026-02-25 15:06:51 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Select elements
 const navToggle = document.querySelector(".nav-toggle");
 const siteNav = document.querySelector(".site-nav");
@@ -7,13 +6,3 @@ const siteNav = document.querySelector(".site-nav");
 navToggle.addEventListener("click", () => {
   siteNav.classList.toggle("open");
 });
-=======
-// Select elements
-const navToggle = document.querySelector(".nav-toggle");
-const siteNav = document.querySelector(".site-nav");
-
-// Toggle menu
-navToggle.addEventListener("click", () => {
-  siteNav.classList.toggle("open");
-});
->>>>>>> 70dddd8631595fa68303ba113bc4bc062f13157d
